5a29a04e9c8bd6c2f6537bb7283422d7ee6b5516
Sundaram03 <124191226+Sundaram03@users.noreply.github.com>
2026-06-15 23:27:23 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDOWctyDAg9rUg9Wr3QtIZoIuXUzuAd1VU",
-  authDomain: "worker-management-82edd.firebaseapp.com",
-  projectId: "worker-management-82edd",
-  storageBucket: "worker-management-82edd.firebasestorage.app",
-  messagingSenderId: "882504843444",
-  appId: "1:882504843444:web:e4decfcb961db3c10097e1",
-  measurementId: "G-6Q51SNFVG3"
+  apiKey: "AIzaSyAUlb5S9OyXaYChJhZvQbk-3CRJd7V-3Yk",
+  authDomain: "product-4752a.firebaseapp.com",
+  projectId: "product-4752a",
+  storageBucket: "product-4752a.firebasestorage.app",
+  messagingSenderId: "717352309307",
+  appId: "1:717352309307:web:d588a93fdb4dd09c08421e",
+  measurementId: "G-YG2W4VG5YV"
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
